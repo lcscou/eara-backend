d9e59a773e482441ed4e91be77dbf5b339c53665
Lucas Coutinho <lcscou@gmail.com>
2025-10-28 21:09:34 -0300

    update

--- wordpress/wp-content/themes/eara/index.php
+++ wordpress/wp-content/themes/eara/index.php
@@ -1,13 +1,13 @@
 <?php get_header(); ?>
 <main id="site-main">
     <?php
-    if ( have_posts() ) :
-        while ( have_posts() ) : the_post();
+    if (have_posts()):
+        while (have_posts()):
+            the_post();
             the_content();
         endwhile;
     endif;
     ?>
 
-    <?php echo print_r($wp_post_types) ;?>
 </main>
 <?php get_footer(); ?>
