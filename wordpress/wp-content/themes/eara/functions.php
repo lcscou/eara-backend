352984fda3be59f08c39c0c31173fa7eaf10a299
Lucas Coutinho <lcscou@gmail.com>
2025-11-19 10:30:41 -0300

    update

--- wordpress/wp-content/themes/eara/functions.php
+++ wordpress/wp-content/themes/eara/functions.php
@@ -5,6 +5,7 @@ include_once TEMPLATEPATH . '/inc/theme.php';
 include_once TEMPLATEPATH . '/inc/blocks.php';
 include_once TEMPLATEPATH . '/inc/graphql-filters.php';
 include_once TEMPLATEPATH . '/inc/image-optimize.php';
+include_once TEMPLATEPATH . '/inc/country-field.php';
 
 
 
--- wordpress/wp-content/themes/eara/inc/graphql-filters.php
+++ wordpress/wp-content/themes/eara/inc/graphql-filters.php
@@ -8,23 +8,65 @@ add_action('graphql_register_types', function () {
         'type' => 'String',
         'description' => 'Filter events by category field',
     ]);
+    register_graphql_field('RootQueryToMediaBankConnectionWhereArgs', 'speciesFeatured', [
+        'type' => 'String',
+        'description' => 'Filter media by species_featured field',
+    ]);
+    register_graphql_field('RootQueryToNewsConnectionWhereArgs', 'category', [
+        'type' => 'String',
+        'description' => 'Filter News by category field',
+    ]);
+    register_graphql_field('RootQueryToNewsConnectionWhereArgs', 'country', [
+        'type' => 'String',
+        'description' => 'Filter News by country field',
+    ]);
+    register_graphql_field('RootQueryToMemberConnectionWhereArgs', 'country', [
+        'type' => 'String',
+        'description' => 'Filter Members by country field',
+    ]);
 });
 add_filter('graphql_post_object_connection_query_args', function ($query_args, $source, $args, $context, $info) {
-    
-    if (isset($args['where']['organizer'])) {
+    if (isset($args['where']['organizer']) && $info->fieldName === 'allEvents') {
         $query_args['meta_query'][] = [
             'key' => 'organizer',
             'value' => $args['where']['organizer'],
             'compare' => '=',
         ];
     }
-   
-  
-    if (isset($args['where']['category'])) {
+    if (isset($args['where']['category']) && $info->fieldName === 'allEvents') {
         $query_args['meta_query'][] = [
             'key' => 'category',
             'value' => $args['where']['category'],
             'compare' => '=',
+        ];
+    }
+    if (isset($args['where']['speciesFeatured']) && $info->fieldName === 'mediaBank') {
+        $query_args['meta_query'][] = [
+            'key' => 'species_featured',
+            'value' => $args['where']['speciesFeatured'],
+            'compare' => '=',
+        ];
+    }
+    if (isset($args['where']['country']) && $info->fieldName === 'allNews') {
+        $query_args['meta_query'][] = [
+            'key' => 'country',
+            'value' => $args['where']['country'],
+            'compare' => 'LIKE',
+        ];
+    }
+    if (isset($args['where']['country']) && $info->fieldName === 'members') {
+        $query_args['meta_query'][] = [
+            'key' => 'country',
+            'value' => $args['where']['country'],
+            'compare' => 'LIKE',
+        ];
+    }
+    // filtrar por taxonomia chamada category-news quando estiver na query news
+    if (isset($args['where']['category']) && $info->fieldName === 'allNews') {
+        $query_args['tax_query'][] = [
+            'taxonomy' => 'categories-news',
+            'field' => 'slug',
+            'terms' => $args['where']['category'],
         ];
     }
     return $query_args;
