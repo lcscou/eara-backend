f1db698240ca8652584ada22151857383a9b6669
Lucas Coutinho <lcscou@gmail.com>
2025-12-02 18:31:13 -0300

    revalidate

--- wordpress/wp-content/themes/eara/inc/nextjs-revalidate.php
+++ wordpress/wp-content/themes/eara/inc/nextjs-revalidate.php
@@ -9,6 +9,17 @@ add_action('delete_post', 'nextjs_revalidate_on_delete');
 // Triggers revalidation on post status transition (draft to publish, etc)
 add_action('transition_post_status', 'nextjs_revalidate_on_status_change', 10, 3);
 
+// Triggers revalidation when post is trashed
+add_action('trashed_post', 'nextjs_revalidate_on_trash');
+
+// Triggers revalidation when post is restored from trash
+add_action('untrashed_post', 'nextjs_revalidate_on_untrash');
+
+// Triggers revalidation when menus are updated
+add_action('wp_update_nav_menu', 'nextjs_revalidate_menus');
+add_action('wp_create_nav_menu', 'nextjs_revalidate_menus');
+add_action('wp_delete_nav_menu', 'nextjs_revalidate_menus');
+
 function nextjs_revalidate_on_save($post_id, $post, $update) {
     // Ignora revisões e auto-saves
     if (wp_is_post_revision($post_id) || wp_is_post_autosave($post_id)) {
@@ -53,14 +64,43 @@ function nextjs_revalidate_on_status_change($new_status, $old_status, $post) {
         return;
     }
     
-    // Revalida quando publicar ou despublicar
-    if ($new_status === 'publish' || $old_status === 'publish') {
+    // Revalida quando publicar, despublicar ou mover para lixeira
+    if ($new_status === 'publish' || $old_status === 'publish' || $new_status === 'trash' || $old_status === 'trash') {
         $path = '/' . $post->post_name;
         $type = $post->post_type;
         
         nextjs_trigger_revalidation($path, $type);
         nextjs_trigger_revalidation('/' . $type, $type);
     }
+}
+
+function nextjs_revalidate_on_trash($post_id) {
+    $post = get_post($post_id);
+    if (!$post) return;
+    
+    $path = '/' . $post->post_name;
+    $type = $post->post_type;
+    
+    // Revalida quando o post vai para a lixeira
+    nextjs_trigger_revalidation($path, $type);
+    nextjs_trigger_revalidation('/' . $type, $type);
+}
+
+function nextjs_revalidate_on_untrash($post_id) {
+    $post = get_post($post_id);
+    if (!$post) return;
+    
+    $path = '/' . $post->post_name;
+    $type = $post->post_type;
+    
+    // Revalida quando o post é restaurado da lixeira
+    nextjs_trigger_revalidation($path, $type);
+    nextjs_trigger_revalidation('/' . $type, $type);
+}
+
+function nextjs_revalidate_menus($menu_id = null) {
+    // Revalida todos os menus
+    nextjs_trigger_revalidation('/menus', 'menus');
 }
 
 function nextjs_trigger_revalidation($path, $type) {
