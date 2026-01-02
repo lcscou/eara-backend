2abd0b6351cba75d81852e18e03864825ebc5af6
Lucas Coutinho <lcscou@gmail.com>
2026-01-02 17:34:26 -0300

    add order filter

--- wordpress/wp-content/themes/eara/inc/graphql-filters.php
+++ wordpress/wp-content/themes/eara/inc/graphql-filters.php
@@ -73,6 +73,96 @@ add_filter('graphql_post_object_connection_query_args', function ($query_args, $
         }
     }
 
+    if ($info->fieldName === 'allEvents' || (isset($query_args['post_type']) && $query_args['post_type'][0] === 'events')) {
+        $events_settings = get_field('events', 'option');
+
+        if ($events_settings) {
+            $orderby = isset($events_settings['orderby-events']) ? $events_settings['orderby-events'] : 'date';
+            $order = isset($events_settings['order-events']) ? $events_settings['order-events'] : 'ASC';
+
+            if ($orderby === 'order') {
+                $query_args['orderby'] = 'meta_value_num';
+                $query_args['meta_key'] = 'order';
+            } else {
+                $query_args['orderby'] = $orderby;
+            }
+
+            $query_args['order'] = $order;
+        }
+    }
+
+    if ($info->fieldName === 'animals' || $info->fieldName === 'allAnimals' || (isset($query_args['post_type']) && $query_args['post_type'][0] === 'animals')) {
+        $animals_settings = get_field('animals', 'option');
+
+        if ($animals_settings) {
+            $orderby = isset($animals_settings['orderby-animals']) ? $animals_settings['orderby-animals'] : 'date';
+            $order = isset($animals_settings['order-animals']) ? $animals_settings['order-animals'] : 'ASC';
+
+            if ($orderby === 'order') {
+                $query_args['orderby'] = 'meta_value_num';
+                $query_args['meta_key'] = 'order';
+            } else {
+                $query_args['orderby'] = $orderby;
+            }
+
+            $query_args['order'] = $order;
+        }
+    }
+
+    if ($info->fieldName === 'allCaseStudies' || $info->fieldName === 'caseStudies' || (isset($query_args['post_type']) && $query_args['post_type'][0] === 'case-studies')) {
+        $case_studies_settings = get_field('case-studies', 'option');
+
+        if ($case_studies_settings) {
+            $orderby = isset($case_studies_settings['orderby-case-studies']) ? $case_studies_settings['orderby-case-studies'] : 'date';
+            $order = isset($case_studies_settings['order-case-studies']) ? $case_studies_settings['order-case-studies'] : 'ASC';
+
+            if ($orderby === 'order') {
+                $query_args['orderby'] = 'meta_value_num';
+                $query_args['meta_key'] = 'order';
+            } else {
+                $query_args['orderby'] = $orderby;
+            }
+
+            $query_args['order'] = $order;
+        }
+    }
+
+    if ($info->fieldName === 'diseases' || $info->fieldName === 'allDiseases' || (isset($query_args['post_type']) && $query_args['post_type'][0] === 'diseases')) {
+        $diseases_settings = get_field('diseases', 'option');
+
+        if ($diseases_settings) {
+            $orderby = isset($diseases_settings['orderby-diseases']) ? $diseases_settings['orderby-diseases'] : 'date';
+            $order = isset($diseases_settings['order-diseases']) ? $diseases_settings['order-diseases'] : 'ASC';
+
+            if ($orderby === 'order') {
+                $query_args['orderby'] = 'meta_value_num';
+                $query_args['meta_key'] = 'order';
+            } else {
+                $query_args['orderby'] = $orderby;
+            }
+
+            $query_args['order'] = $order;
+        }
+    }
+
+    if ($info->fieldName === 'mediasBank' || $info->fieldName === 'mediaBank' || $info->fieldName === 'allMediaBank' || (isset($query_args['post_type']) && $query_args['post_type'][0] === 'media-bank')) {
+        $media_bank_settings = get_field('media-bank', 'option');
+
+        if ($media_bank_settings) {
+            $orderby = isset($media_bank_settings['orderby-media-bank']) ? $media_bank_settings['orderby-media-bank'] : 'date';
+            $order = isset($media_bank_settings['order-media-bank']) ? $media_bank_settings['order-media-bank'] : 'ASC';
+
+            if ($orderby === 'order') {
+                $query_args['orderby'] = 'meta_value_num';
+                $query_args['meta_key'] = 'order';
+            } else {
+                $query_args['orderby'] = $orderby;
+            }
+
+            $query_args['order'] = $order;
+        }
+    }
+
     if (isset($args['where']['organizer']) && $info->fieldName === 'allEvents') {
         $query_args['meta_query'][] = [
             'key' => 'organizer',
